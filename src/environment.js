98187ce088ab2bf8dab8f22985ca3615a041627d
Binod Chaudhary <binod@rumsan.com>
2021-04-26 11:16:44 +0545

    update enironment

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,4 @@
-let BASE_URL = 'https://dev-api.avendi.me/api';
+let BASE_URL = 'https://dev-api.avendi.me/barranca/api';
 
 if (process.env.REACT_APP_ENV === 'local') {
   BASE_URL = 'http://localhost:4000/api';
